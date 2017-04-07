5770552ed898c0f08c6598872befbd6a942aeebf
Alexis Lui <alexislui@hotmail.co.uk>
2017-04-07 11:49:42 +0100

    Included a fake config.env file

--- src/routes/post_submit.js
+++ src/routes/post_submit.js
@@ -4,16 +4,12 @@ module.exports = {
   method: 'POST',
   path: '/post/submit',
   handler: (request, reply) => {
-<<<<<<< HEAD
+
     postJoke('Oli', request.payload.joke, (err, res) => {
       if (err) {
         return reply.redirect('Something went wrong sorry!');
       }
       else {
-=======
-    post_joke('Oli', request.payload.joke, (err, res) => {
-      if (err) console.log(err);
->>>>>>> 24b702a8e296b475133c1718c916342a45169314
       return reply.redirect('/');
       }
     })
