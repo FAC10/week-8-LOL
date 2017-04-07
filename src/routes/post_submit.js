adec44f0b3f4109db89fbb977f67608013bd750f
lucyrose93 <lucyrose@protonmail.com>
2017-04-07 11:22:16 +0100

    cleaning up files
    changed postJoke to snake case -- relates #75. Deleted empty file test.hbs. relates #72. Removed comments in header.hbs -- relates #71

--- src/routes/post_submit.js
+++ src/routes/post_submit.js
@@ -1,10 +1,10 @@
-const postJoke = require('../postJoke');
+const post_joke = require('../post_joke');
 
 module.exports = {
   method: 'POST',
   path: '/post/submit',
   handler: (request, reply) => {
-    postJoke('Oli', request.payload.joke, (err, res) => {
+    post_joke('Oli', request.payload.joke, (err, res) => {
       if (err) console.log(err);
       return reply.redirect('/');
     })
