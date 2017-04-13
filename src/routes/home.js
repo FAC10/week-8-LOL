7bb41aad7499d9bb0a2f0093dde0322698f727aa
oliverjam <oliverjamesphillips@gmail.com>
2017-04-13 11:51:54 +0100

    Only check usernames if user is actually logged in
    Relates #44

--- src/routes/home.js
+++ src/routes/home.js
@@ -9,14 +9,13 @@ module.exports = {
         return reply('Something went wrong sorry!');
       }
       const options = { jokes };
-      jokes.forEach((joke) => {
-        if (joke.username === req.auth.credentials.username) {
-          joke.self = true;
-        }
-      });
-      console.log(jokes);
       if (req.auth.isAuthenticated) {
         options.credentials = req.auth.credentials;
+        jokes.forEach((joke) => {
+          if (joke.username === req.auth.credentials.username) {
+            joke.self = true;
+          }
+        });
       }
       reply.view('index', options);
     });
